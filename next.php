a7f9fc8ef0959df0b854277293ce03bc923ba8b8
TobeyG2003 <158863158+TobeyG2003@users.noreply.github.com>
2024-03-31 14:08:15 -0400

    Update next.php

--- next.php
+++ next.php
@@ -1,11 +1,11 @@
 <?php session_start(); /* Starts the session */
-include 'common.php';
-$_SESSION['letters']='';
-$_SESSION['score'] += $_SESSION['attempts'];
-$_SESSION['attempts'] = 6;
-$_SESSION['word_id'] = getWord($_SESSION['difficulty'],rand(0,9));
-$_SESSION['blank'] = getBlank($_SESSION['word_id']);
-$_SESSION['warning'] ='';
-header("location:formurltest.php"); /*return to page*/
-exit;
+  include 'common.php'; //include functions
+  $_SESSION['letters']=''; //reset entered letters
+  $_SESSION['score'] += $_SESSION['attempts']; //add remaining attempts to the score
+  $_SESSION['attempts'] = 6; //reset attempts
+  $_SESSION['word_id'] = getWord($_SESSION['difficulty'],rand(0,9)); //get a new word
+  $_SESSION['blank'] = getBlank($_SESSION['word_id']); //get new blanks
+  $_SESSION['warning'] =''; //reset warning
+  header("location:formurltest.php"); /*return to page*/
+  exit;
 ?>
