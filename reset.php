706433594f4dbff8aa86f3603ade02f615715c8d
TobeyG2003 <158863158+TobeyG2003@users.noreply.github.com>
2024-03-31 14:06:55 -0400

    Update reset.php

--- reset.php
+++ reset.php
@@ -1,5 +1,5 @@
 <?php session_start(); /* Starts the session */
-session_destroy();
-header("location:start.php"); /*return to page*/
-exit;
+  session_destroy(); //destroy session i.e. reset all session variables
+  header("location:start.php"); /*return to start page*/
+  exit;
 ?>
