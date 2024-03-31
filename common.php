655b72b02f3ab737d3c4dcad4b6ed7287ec87ade
TobeyG2003 <158863158+TobeyG2003@users.noreply.github.com>
2024-03-31 14:14:07 -0400

    Update common.php

--- common.php
+++ common.php
@@ -1,28 +1,28 @@
 <?php
-	function getWord ($diff, $i) {
-		$easy = array("good","boat","jump","time","ride","car","home","plan","film","web");
-		$medium = array("candor","despot","emulate","program","brazen","caveat","poise","strength","strategy","verdict");
-		$hard = array("trepidation","mausoleum","insidious","incorrigible","vacillate","camaraderie","iridescent","magnitude","aberration","bombastic");
-		if ($diff == "easy") {
-			return str_split($easy[$i]);
-		} else if ($diff == "normal") {
-			return str_split($medium[$i]);
-		} else {
-			return str_split($hard[$i]);
+	function getWord ($diff, $i) { //function to get a word
+		$easy = array("good","boat","jump","time","ride","car","home","plan","film","web"); //list of easy difficulty words
+		$medium = array("candor","despot","emulate","program","brazen","caveat","poise","strength","strategy","verdict"); //list of normal difficulty words
+		$hard = array("trepidation","mausoleum","insidious","incorrigible","vacillate","camaraderie","iridescent","magnitude","aberration","bombastic"); //list of hard difficulty words
+		if ($diff == "easy") { //if submitted difficulty var was easy
+			return str_split($easy[$i]); //return an easy word (as an array of letters) from index of easy array ($i is random 0-9 when the function is called)
+		} else if ($diff == "normal") { //if diff is normal
+			return str_split($medium[$i]); //return nommal word
+		} else { //if diff is hard
+			return str_split($hard[$i]); //return hard
 		}
 	}
 	
-	function getBlank ($word) {
-		$blank = array();
-		foreach ($word as $space) {
-			$blank[] = '_';
+	function getBlank ($word) { //function to get blank spaces (filled in with letters during the game)
+		$blank = array(); //create array
+		foreach ($word as $space) { //for each letter in the word
+			$blank[] = '_'; //add a blank space, or an underline 
 		}
-		return $blank;
+		return $blank; //return array
 	}
 	
-	function printArray($word) {
-		foreach ($word as $val) {
-			echo $val . ' ';
+	function printArray($word) { //function to print the words/spaces in the game without print_r information
+		foreach ($word as $val) { //for each letter in the word
+			echo $val . ' '; //print letter, add a space for clarity, 
 		}
 	}
 ?>
