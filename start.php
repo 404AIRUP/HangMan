7faaa49a4af473f426d2e7970377edd1e129bbf8
TobeyG2003 <158863158+TobeyG2003@users.noreply.github.com>
2024-03-31 14:06:09 -0400

    Update start.php

--- start.php
+++ start.php
@@ -1,14 +1,13 @@
 <?php
-include 'common.php';
-session_start();
-if (isset($_POST['Submit'])) {
-    $_SESSION['score'] = 0;
-    $_SESSION['difficulty'] = $_POST['diff'];
-    $_SESSION['word_id'] = getWord($_SESSION['difficulty'], rand(0, 9));
-    $_SESSION['blank'] = getBlank($_SESSION['word_id']);
-    $_SESSION['attempts'] = 6;
-    $_SESSION['test'] = 'test';
-    header("location:formurltest.php"); //redirect to getting matched with query parameter from submit
+include 'common.php'; //include functions
+session_start(); //start session to record session vars
+if (isset($_POST['Submit'])) { //check if submit is entered
+    $_SESSION['score'] = 0; //start score from 0
+    $_SESSION['difficulty'] = $_POST['diff']; //read difficulty input from form and enter session var
+    $_SESSION['word_id'] = getWord($_SESSION['difficulty'], rand(0, 9)); //using the set session var difficulty and a random number, get a word from array of respective difficulty words, store as session array var of chars
+    $_SESSION['blank'] = getBlank($_SESSION['word_id']); //using the array of chars, generate an equally long array of underlines
+    $_SESSION['attempts'] = 6; //set base attempts of 6
+    header("location:formurltest.php"); //redirect to game after submit
     exit;
 }
 ?>
