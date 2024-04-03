99882ff9520387bb790fd24a4e242ee7cced3874
TobeyG2003 <158863158+TobeyG2003@users.noreply.github.com>
2024-04-02 21:18:37 -0400

    Update next.php

--- next.php
+++ next.php
@@ -1,6 +1,6 @@
 <?php session_start(); /* Starts the session */
   include 'common.php'; //include functions
-  $_SESSION['letters']=''; //reset entered letters
+  $_SESSION['letters']=array(); //reset entered letters
   $_SESSION['score'] += $_SESSION['attempts']; //add remaining attempts to the score
   $_SESSION['attempts'] = 6; //reset attempts
   $_SESSION['word_id'] = getWord($_SESSION['difficulty'],rand(0,9)); //get a new word
