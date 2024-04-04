1139258128e01e73991f95c9c64e2ac26ced265e
TobeyG2003 <158863158+TobeyG2003@users.noreply.github.com>
2024-04-03 22:33:35 -0400

    Update win.php

--- win.php
+++ win.php
@@ -4,3 +4,18 @@ if(!(isset($_SESSION['name'])) || ($_SESSION['rounds'] != 6)){
 	exit;
 }
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Hangman Game<title>
+</head>
+<body>
+	<div class = "congrats">Congratulations!</div>
+	<div class = "wintext">You Won!</div>
+	<div class= "score">Your Score: <?= $_SESSION['score'] ?> </div>
+	<div class = "prev">
+		Previous Users: <?= //insert scoreboard ?>
+	</div>
+	<div class = "back"><a href="./reset.php">Return to Start?</a></div>
+</body>
+</html>
